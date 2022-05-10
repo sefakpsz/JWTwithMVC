c030b6a61539182d3896300dc8edbb6dc7636038
sefakpsz <m.sefa06@hotmail.com>
2022-05-10 13:22:44 +0300

    Add project files.

---
+++ JWTwithMVC/Controllers/SecurityController.cs
@@ -0,0 +1,74 @@
+﻿
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
+
+namespace JWTwithMVC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public SecurityController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private string GenerateJSONWebToken(string username)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim("Issuer", "sefa"),
+                new Claim("Admin","true"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(JwtRegisteredClaimNames.UniqueName, username)
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: "sefa",
+                audience: "sefa",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+        // GET: api/<SecurityController>
+
+        // GET api/<SecurityController>/5
+        [HttpGet]
+        public string Get()
+        {
+            return GenerateJSONWebToken("sefa");
+        }
+
+        // POST api/<SecurityController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SecurityController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SecurityController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ JWTwithMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JWTwithMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
